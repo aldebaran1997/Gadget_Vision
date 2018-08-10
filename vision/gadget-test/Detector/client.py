efa70a8788a8ed4584ce505c894c86f288dcc1a3
prk3128 <prk3128@gmail.com>
2018-08-10 16:36:22 +0900

    Revise Client.py

--- vision/gadget-test/Detector/client.py
+++ vision/gadget-test/Detector/client.py
@@ -4,14 +4,12 @@ import time
 
 class TrasnferClient() :
 
-    def __init__(self,ServerIP='192.168.0.2',PORT=5010,read_path='/home/cae/Desktop/',filename='result.png') :
+    def __init__(self,ServerIP='192.168.0.2',PORT=5010) :
 
-        self.read_path = read_path
-        self.filename = filename
         self.ServerIP = ServerIP
         self.PORT = PORT
 
-    def Transfer(self) :
+    def Transfer(self,read_path='/home/cae/Desktop/',filename='result.png') :
 
         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -20,9 +18,9 @@ class TrasnferClient() :
             sock.connect((self.ServerIP, self.PORT))
             print("Server : ", self.ServerIP, self.PORT)
 
-        file = open( self.read_path+self.filename,'rb')
+        file = open( read_path+filename,'rb')
 
-        file_size = os.path.getsize(self.read_path+self.filename)
+        file_size = os.path.getsize(read_path+filename)
 
         data = file.read(file_size)
 
